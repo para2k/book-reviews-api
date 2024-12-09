69622bc0109241bd9179958e8c1db8f998006ea2
para2k <132300149+para2k@users.noreply.github.com>
2024-12-09 19:58:54 +0100

    Modified Book model to use aggregation

--- models/Book.js
+++ models/Book.js
@@ -13,15 +13,26 @@ const BookSchema = new mongoose.Schema({
     description: {
         type: String,
     },
-    averageRating: {
-        type: Number,
-        default: 0,
-        min: 0,
-        max: 5,
-    },
+    reviews: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Review",
+        }
+    ]
 }, {
     timestamps: true // adds createdAt and updatedAt timestamps
 });
+
+//Create average rating for the book using aggregation
+BookSchema.methods.calculateAverageRating = async function () {
+    const result = await Review.aggregate([
+        { $match: { book: this._id } },
+        { $group: { _id: "$book", avgRating: { $avg: "$rating" } } }
+    ]);
+
+    if(result.length === 0) return 0; 
+    return result[0].avgRating.toFixed(2);
+};
 
 module.exports = mongoose.model("Book", BookSchema);
     
